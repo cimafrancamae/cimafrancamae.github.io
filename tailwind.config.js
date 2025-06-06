12072719adc17e9cf1bd5594aa365b2c8efd5b08
maeci <cimafrancamae@gmail.com>
2025-06-06 16:19:21 +0800

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        beige: "#F9F5EF",       // soft neutral background
+        clay: "#836953",        // warm muted brown
+        sage: "#DDE2C6",        // gentle green
+        terracotta: "#D7745B",  // rich earthy orange
+        brown: "#4E3629",       // strong dark base
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"], // or your preferred modern font
+      },
+    },
   },
   plugins: [],
-}
-
+};
